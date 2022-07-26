f9cab1b9237091984add0a6b7ad8ab114fef5e67
anabxathag <109953263+anabxathag@users.noreply.github.com>
2022-07-26 12:37:11 +0700

    Add files via upload

---
+++ 4d2.py
@@ -0,0 +1,40 @@
+'''[Onsite Day 2] Math Symbol'''
+def main():
+    '''ผลลัพธ์ ทศนิยม 2 ตำเเหน่ง'''
+    var_list = []
+    while True:
+        num = input()
+        if num == "+":
+            print("%.2f" %(sum(var_list)))
+            break
+        elif num == "-":
+            print("%.2f" %(diff(var_list)))
+            break
+        elif num == "*":
+            print("%.2f" %(mul(var_list)))
+            break
+        elif num == "/":
+            print("%.2f" %(sub(var_list)))
+            break
+        else:
+            var_list += [float(num)]
+def diff(varlist):
+    '''-'''
+    first = varlist.pop(0)
+    for iso in varlist:
+        first -= iso
+    return first
+def mul(varlist):
+    '''*'''
+    first = varlist.pop(0)
+    for iso in varlist:
+        first *= iso
+    return first
+def sub(varlist):
+    '''/'''
+    first = varlist.pop(0)
+    for iso in varlist:
+        first /= iso
+    return first
+
+main()
